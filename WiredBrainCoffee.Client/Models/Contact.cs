05cc2593c2274675ee75a1fb17c33244910bd1de
Alex Wolf <alexrudiwolf@gmail.com>
2020-11-01 17:44:42 -0500

    initial commit

---
+++ WiredBrainCoffee.Client/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Phone]
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [MinLength(10)]
+        [Required]
+        public string Message { get; set; }
+    }
+}
